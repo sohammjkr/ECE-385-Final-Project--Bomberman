edc45f303b7a4be9dcdb4ab9d66c338045136160
tnv2 <103698371+tnv2@users.noreply.github.com>
2022-04-15 19:26:14 -0500

    added files for sprite

---
+++ background_ram.sv
@@ -0,0 +1,32 @@
+/*
+ * ECE385-HelperTools/PNG-To-Txt
+ * Author: Rishi Thakkar
+ *
+ */
+
+module  frameROM
+(
+		//input [4:0] data_In,
+		//input [18:0] write_address, read_address,
+		input [18:0] read_address,
+		input we, Clk,
+
+		output logic [4:0] data_Out
+);
+
+// mem has width of 3 bits and a total of 400 addresses
+logic [4:0] mem [0:307199];
+
+initial
+begin
+	 $readmemh("sprite_bytes/background_new.txt", mem);
+end
+
+
+always_ff @ (posedge Clk) begin
+	//if (we)
+		//mem[write_address] <= data_In;
+	data_Out<= mem[read_address];
+end
+
+endmodule
